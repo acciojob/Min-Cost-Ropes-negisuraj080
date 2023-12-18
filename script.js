32eeccf5a5508170f2b17cbf5be6d8aec76f03e6
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-18 17:34:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-function mincost(arr)
-{ 
-	totalCost = 0;
-	arr.sort((a, b) => b - a);
- while (arr.length > 1) { // (n - 1)
-     let currentCost = arr[arr.length - 1] + arr[arr.length - 2];
-     totalCost += currentCost;
-     arr.splice(0, 2);
-     arr.pop(); arr.pop();
-     arr.push(currentCost);
-     arr.sort((a, b) => b - a); // (nlogn)
- }
-	return totalCost;
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+  let totalCost = 0;
+
+  // Continue until there is only one rope left
+  while (arr.length > 1) {
+    // Sort the array in ascending order (smallest ropes first)
+    arr.sort((a, b) => a - b);
+
+    // Take the two smallest ropes
+    const firstMin = arr.shift();
+    const secondMin = arr.shift();
+
+    // Calculate the current cost
+    const currentCost = firstMin + secondMin;
+
+    // Add the current cost to the total
+    totalCost += currentCost;
+
+    // Insert the combined rope back into the array
+    arr.push(currentCost);
+  }
+
+  return totalCost;
 }
 
 module.exports=mincost;
